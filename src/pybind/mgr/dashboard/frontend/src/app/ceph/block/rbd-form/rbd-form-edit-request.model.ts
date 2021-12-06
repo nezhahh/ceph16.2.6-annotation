109491f93ac8d3483bf59259b9aeaea431aa3fbf
nezhahh <53523984+nezhahh@users.noreply.github.com>
2021-12-06 14:11:36 +0800

    Delete src/pybind directory
    删除pybind

--- src/pybind/mgr/dashboard/frontend/src/app/ceph/block/rbd-form/rbd-form-edit-request.model.ts
+++
@@ -1,8 +0,0 @@
-import { RbdConfigurationEntry } from '~/app/shared/models/configuration';
-
-export class RbdFormEditRequestModel {
-  name: string;
-  size: number;
-  features: Array<string> = [];
-  configuration: RbdConfigurationEntry[];
-}
