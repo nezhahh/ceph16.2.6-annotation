7a329a04cd0317c9ee70f9d4d7a7828b0993eded
xh.dong <xh.dong@shunwang.com>
2021-12-06 13:48:16 +0800

    原始全量代码

---
+++ src/pybind/mgr/dashboard/frontend/src/app/ceph/rgw/models/rgw-daemon.ts
@@ -0,0 +1,9 @@
+export class RgwDaemon {
+  id: string;
+  service_map_id: string;
+  version: string;
+  server_hostname: string;
+  zonegroup_name: string;
+  zone_name: string;
+  default: boolean;
+}
